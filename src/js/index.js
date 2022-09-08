49a20fe2ba325885196de8471bde4b4ebfa29921
Bennett Smrdel <bennett.smrdel@heysparkbox.com>
2022-09-08 08:13:08 -0500

    feat: added renderCharacter file
    - Created a new directory for views.
    - Added renderCharacter.js to the directory.
    - Creating and appending hero card element to DOM.
    - All content besides image is being rendered.
    - Replaced html encoding with string interpolation, to get the image.
    
    FSA22V2-81

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,10 @@ import '@/assets/fonts/Bangers/index.scss';
 import '@/assets/fonts/LeagueSpartan/index.scss';
 import '@/assets/scss/base.scss';
 import { getCharacterById } from '@/js/services/characterService.js';
+import renderCharacters from './views/renderCharacter';
 
 (async () => {
   const { name: characterName } = await getCharacterById(70);
   console.debug(`%cNa na na na na na na na na na na na na, ${characterName}!`, 'font-family: Menlo, monospace; color: yellow;"');
+  renderCharacters();
 })();
