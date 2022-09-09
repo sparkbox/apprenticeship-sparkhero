625752b380b57ce287c418a4b9c7112e18da3c2f
Jared Kohrt <jared.kohrt@heysparkbox.com>
2022-09-09 11:38:51 -0400

    feat: add getFilteredCharactersByAlignment
    Add a method that takes in a hero array and returns one with only the
    specified alignment ('good' or 'bad')

--- src/js/utilities/characterFilters.js
+++ src/js/utilities/characterFilters.js
@@ -1,3 +1,12 @@
+/*  getFilteredCharactersByAlignment
+*   @param {[Character]} Character array to filter
+*   @param {String}   Alignment of characters in output arr
+*/
+export function getFilteredCharactersByAlignment(characters, alignment) {
+  const result = characters.filter((char) => char.biography.alignment === alignment);
+  return result;
+}
+
 // Takes an array of characters, and a publisher to filter by.
 // Returns an array of characters filtered by publisher.
 export function getFilteredCharactersByPublisher(arr, publisher) {
---
+++ src/js/utilities/characterFilters.spec.js
@@ -0,0 +1,36 @@
+import { getFilteredCharactersByAlignment } from './characterFilters';
+
+describe('getFilteredCharactersByAlignment', () => {
+  const characters = [
+    {
+      id: 3,
+      name: 'Abin Sur',
+      biography: {
+        alignment: 'good',
+      },
+    },
+    {
+      id: 4,
+      name: 'Abomination',
+      biography: {
+        alignment: 'bad',
+      },
+    },
+    {
+      id: 5,
+      name: 'Abraxas',
+      biography: {
+        alignment: 'bad',
+      },
+    }];
+
+  it('returns a filtered array of only "good" characters', () => {
+    const goodCharacters = getFilteredCharactersByAlignment(characters, 'good');
+    expect(goodCharacters).toEqual([characters[0]]);
+  });
+
+  it('returns a filtered array of only "bad" characters', () => {
+    const badCharacters = getFilteredCharactersByAlignment(characters, 'bad');
+    expect(badCharacters).toEqual([characters[1], characters[2]]);
+  });
+});
