059ddc846dcb84f667b2977d2acdf13033a8445e
Bennett Smrdel <bennett.smrdel@heysparkbox.com>
2022-09-09 09:20:25 -0500

    feat: Created characterSort.js file.
    - Added the characterSort.js file.
    - Created the getSorted characters function.
    - The function takes in an array and string.
    - The switch statement returns an array based on the string.
    
    FSA22V2-87

---
+++ src/js/utilities/characterSort.js
@@ -0,0 +1,7 @@
+// Returns an sorted array of characters, depending on the string input.
+export function getSortedCharacters(characters, sort) {
+  switch (sort) {
+    default:
+      return characters;
+  }
+}
