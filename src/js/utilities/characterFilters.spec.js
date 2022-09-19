172699ed8498b82d0f79c703081a449cb0caa3d5
Jared Kohrt <jared.kohrt@heysparkbox.com>
2022-09-19 14:01:05 -0400

    feat: add getSortedByName to characterSort.js

--- src/js/utilities/characterFilters.spec.js
+++ src/js/utilities/characterFilters.spec.js
@@ -1,62 +1,69 @@
-import { getFilteredCharactersByAlignment } from './characterFilters';
-import { getFilteredCharactersByPublisher } from './characterFilters';
+import { getFilteredCharactersByAlignment, getFilteredCharactersByPublisher } from './characterFilters';
 
-const characters = [
-  {
-    id: 1,
-    name: 'A-Bomb',
-    slug: '1-a-bomb',
-    powerstats: {
-      intelligence: 38, strength: 100, speed: 17, durability: 80, power: 24, combat: 64,
+function makeTestArray() {
+  return [
+    {
+      id: 1,
+      name: 'A-Bomb',
+      slug: '1-a-bomb',
+      powerstats: {
+        intelligence: 38, strength: 100, speed: 17, durability: 80, power: 24, combat: 64,
+      },
+      appearance: {
+        gender: 'Male', race: 'Human', height: ['6\'8', '203 cm'], weight: ['980 lb', '441 kg'], eyeColor: 'Yellow', hairColor: 'No Hair',
+      },
+      biography: {
+        fullName: 'Richard Milhouse Jones', alterEgos: 'No alter egos found.', aliases: ['Rick Jones'], placeOfBirth: 'Scarsdale,  Arizona', firstAppearance: 'Hulk Vol 2 #2 (April,  2008) (as A-Bomb)', publisher: 'Marvel Comics', alignment: 'good',
+      },
+      work: {
+        occupation: 'Musician,  adventurer,  author; formerly talk show host', base: '-',
+      },
+      connections: {
+        groupAffiliation: 'Hulk Family; Excelsior (sponsor), Avengers (honorary member); formerly partner of the Hulk, Captain America and Captain Marvel; Teen Brigade; ally of Rom', relatives: 'Marlo Chandler-Jones (wife); Polly (aunt); Mrs. Chandler (mother-in-law); Keith Chandler, Ray Chandler, three unidentified others (brothers-in-law); unidentified father (deceased); Jackie Shorr (alleged mother; unconfirmed)',
+      },
     },
-    appearance: {
-      gender: 'Male', race: 'Human', height: ['6\'8', '203 cm'], weight: ['980 lb', '441 kg'], eyeColor: 'Yellow', hairColor: 'No Hair',
+    {
+      id: 2,
+      name: 'Evil-A-Bomb',
+      slug: '2-b-bomb',
+      powerstats: {
+        intelligence: 38, strength: 100, speed: 17, durability: 80, power: 24, combat: 64,
+      },
+      appearance: {
+        gender: 'Male', race: 'Human', height: ['6\'8', '203 cm'], weight: ['980 lb', '441 kg'], eyeColor: 'Yellow', hairColor: 'No Hair',
+      },
+      biography: {
+        fullName: 'Richard Milhouse Jones', alterEgos: 'No alter egos found.', aliases: ['Rick Jones'], placeOfBirth: 'Scarsdale,  Arizona', firstAppearance: 'Hulk Vol 2 #2 (April,  2008) (as A-Bomb)', publisher: 'Not Marvel Comics', alignment: 'bad',
+      },
+      work: {
+        occupation: 'Musician,  adventurer,  author; formerly talk show host', base: '-',
+      },
+      connections: {
+        groupAffiliation: 'Hulk Family; Excelsior (sponsor), Avengers (honorary member); formerly partner of the Hulk, Captain America and Captain Marvel; Teen Brigade; ally of Rom', relatives: 'Marlo Chandler-Jones (wife); Polly (aunt); Mrs. Chandler (mother-in-law); Keith Chandler, Ray Chandler, three unidentified others (brothers-in-law); unidentified father (deceased); Jackie Shorr (alleged mother; unconfirmed)',
+      },
     },
-    biography: {
-      fullName: 'Richard Milhouse Jones', alterEgos: 'No alter egos found.', aliases: ['Rick Jones'], placeOfBirth: 'Scarsdale,  Arizona', firstAppearance: 'Hulk Vol 2 #2 (April,  2008) (as A-Bomb)', publisher: 'Marvel Comics', alignment: 'good',
-    },
-    work: {
-      occupation: 'Musician,  adventurer,  author; formerly talk show host', base: '-',
-    },
-    connections: {
-      groupAffiliation: 'Hulk Family; Excelsior (sponsor), Avengers (honorary member); formerly partner of the Hulk, Captain America and Captain Marvel; Teen Brigade; ally of Rom', relatives: 'Marlo Chandler-Jones (wife); Polly (aunt); Mrs. Chandler (mother-in-law); Keith Chandler, Ray Chandler, three unidentified others (brothers-in-law); unidentified father (deceased); Jackie Shorr (alleged mother; unconfirmed)',
-    },
-  },
-  {
-    id: 2,
-    name: 'Evil-A-Bomb',
-    slug: '2-b-bomb',
-    powerstats: {
-      intelligence: 38, strength: 100, speed: 17, durability: 80, power: 24, combat: 64,
-    },
-    appearance: {
-      gender: 'Male', race: 'Human', height: ['6\'8', '203 cm'], weight: ['980 lb', '441 kg'], eyeColor: 'Yellow', hairColor: 'No Hair',
-    },
-    biography: {
-      fullName: 'Richard Milhouse Jones', alterEgos: 'No alter egos found.', aliases: ['Rick Jones'], placeOfBirth: 'Scarsdale,  Arizona', firstAppearance: 'Hulk Vol 2 #2 (April,  2008) (as A-Bomb)', publisher: 'Not Marvel Comics', alignment: 'bad',
-    },
-    work: {
-      occupation: 'Musician,  adventurer,  author; formerly talk show host', base: '-',
-    },
-    connections: {
-      groupAffiliation: 'Hulk Family; Excelsior (sponsor), Avengers (honorary member); formerly partner of the Hulk, Captain America and Captain Marvel; Teen Brigade; ally of Rom', relatives: 'Marlo Chandler-Jones (wife); Polly (aunt); Mrs. Chandler (mother-in-law); Keith Chandler, Ray Chandler, three unidentified others (brothers-in-law); unidentified father (deceased); Jackie Shorr (alleged mother; unconfirmed)',
-    },
-  },
-];
+  ];
+}
 
 describe('characterFilters.js', () => {
-  it('Should return an array of characters filtered by publisher', () => {
-    const filteredList = getFilteredCharactersByPublisher(characters, 'Marvel Comics');
-    expect(filteredList).toEqual([characters[0]]);
+  it('Should return an array of Characters filtered by publisher', () => {
+    const filteredList = getFilteredCharactersByPublisher(makeTestArray(), 'Marvel Comics');
+    filteredList.forEach((element) => {
+      expect(element.biography.publisher).toBe('Marvel Comics');
+    });
   });
 
-  it('returns a filtered array of only the "good" characters from an array of characters', () => {
-    const goodCharacters = getFilteredCharactersByAlignment(characters, 'good');
-    expect(goodCharacters).toEqual([characters[0]]);
+  it('returns a filtered array of only the "good" Characters from an array of characters', () => {
+    const goodCharacters = getFilteredCharactersByAlignment(makeTestArray(), 'good');
+    goodCharacters.forEach((element) => {
+      expect(element.biography.alignment).toBe('good');
+    });
   });
 
-  it('returns a filtered array of only the "bad" characters from an array of characters', () => {
-    const badCharacters = getFilteredCharactersByAlignment(characters, 'bad');
-    expect(badCharacters).toEqual([characters[1]]);
+  it('returns a filtered array of only the "bad" Characters from an array of characters', () => {
+    const badCharacters = getFilteredCharactersByAlignment(makeTestArray(), 'bad');
+    badCharacters.forEach((element) => {
+      expect(element.biography.alignment).toBe('bad');
+    });
   });
 });
--- src/js/utilities/characterSort.js
+++ src/js/utilities/characterSort.js
@@ -1,6 +1,20 @@
-// Returns an sorted array of characters, depending on the string input.
+function getCharactersSortedByName(characters) {
+  return characters.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1; // a goes before b
+    }
+    if (a.name > b.name) {
+      return 1; // a goes after b
+    }
+    return 0; // a and b aren't moved
+  });
+}
+
+// Returns an sorted array of Character objects, depending on the string input.
 export function getSortedCharacters(characters, sort) {
   switch (sort) {
+    case 'name':
+      return getCharactersSortedByName(characters);
     default:
       return characters;
   }
